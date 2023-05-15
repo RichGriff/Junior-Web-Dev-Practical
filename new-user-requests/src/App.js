26392569bdc342d1c3755e39721194707909106f
Rich Griffiths <rich.griffiths89@gmail.com>
2023-05-15 12:48:02 +0100

    New Request Added

--- new-user-requests/src/App.js
+++ new-user-requests/src/App.js
@@ -1,17 +1,27 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState, Fragment } from "react";
+import { Dialog, Transition } from '@headlessui/react'
 import './App.css';
 import data from './data/data.json'
 import RequestCard from './components/RequestCard';
 import { filterOptions } from './data/filterOptions'
+import NewRequestForm from "./components/NewRequestForm";
 
 function App() {
-  const [showForm, setShowForm] = useState(false)
+  let [isOpen, setIsOpen] = useState(false)
   const [requests, setRequests] = useState([]);
   const [selectedBusinessArea, setSelectedBusinessArea] = useState();
 
   useEffect(() => {
     setRequests(data);
   }, []);
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
 
   function getFilteredList() {
     if (!selectedBusinessArea) {
@@ -35,7 +45,7 @@ function App() {
       {/* New User Request Form */}
       <div className='w-full h-16 flex justify-between items-center my-4'>
         <img src='/pobl-logo.png' className="w-28" alt="logo" />
-        <button onClick={() => setShowForm(!showForm)} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white hover:text-white bg-pink-500 rounded-lg hover:bg-pink-600 transition-all focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+        <button onClick={openModal} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white hover:text-white bg-pink-500 rounded-lg hover:bg-pink-600 transition-all focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-4 h-4 mr-1">
               <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
             </svg>
@@ -43,10 +53,46 @@ function App() {
         </button>
       </div>
 
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-lg transform overflow-hidden rounded-2xl bg-white p-2 text-left align-middle shadow-xl transition-all">
+                  <NewRequestForm closeModal={closeModal} requests={requests} setRequests={setRequests} />
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+
+      {/* List Filter Options */}
       <div className='flex justify-center items-center gap-4'>
         {filterOptions.map((item, index) => (
           <button
-            id={index}
+            id={item.id}
+            key={index}
             onClick={handleBusinessChange}
             className={`${(selectedBusinessArea === item.value) ? 'bg-violet-500 text-white hover:bg-violet-400' : 'bg-slate-200 text-slate-800 hover:bg-slate-300'} rounded-md py-1 px-3`}
           >
@@ -65,3 +111,78 @@ function App() {
 }
 
 export default App;
+
+
+
+// export default function MyModal() {
+
+//   return (
+//     <>
+//       <div className="fixed inset-0 flex items-center justify-center">
+//         <button
+//           type="button"
+//           onClick={openModal}
+//           className="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+//         >
+//           Open dialog
+//         </button>
+//       </div>
+
+//       <Transition appear show={isOpen} as={Fragment}>
+//         <Dialog as="div" className="relative z-10" onClose={closeModal}>
+//           <Transition.Child
+//             as={Fragment}
+//             enter="ease-out duration-300"
+//             enterFrom="opacity-0"
+//             enterTo="opacity-100"
+//             leave="ease-in duration-200"
+//             leaveFrom="opacity-100"
+//             leaveTo="opacity-0"
+//           >
+//             <div className="fixed inset-0 bg-black bg-opacity-25" />
+//           </Transition.Child>
+
+//           <div className="fixed inset-0 overflow-y-auto">
+//             <div className="flex min-h-full items-center justify-center p-4 text-center">
+//               <Transition.Child
+//                 as={Fragment}
+//                 enter="ease-out duration-300"
+//                 enterFrom="opacity-0 scale-95"
+//                 enterTo="opacity-100 scale-100"
+//                 leave="ease-in duration-200"
+//                 leaveFrom="opacity-100 scale-100"
+//                 leaveTo="opacity-0 scale-95"
+//               >
+//                 <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+//                   <Dialog.Title
+//                     as="h3"
+//                     className="text-lg font-medium leading-6 text-gray-900"
+//                   >
+//                     Payment successful
+//                   </Dialog.Title>
+//                   <div className="mt-2">
+//                     <p className="text-sm text-gray-500">
+//                       Your payment has been successfully submitted. We’ve sent
+//                       you an email with all of the details of your order.
+//                     </p>
+//                   </div>
+
+//                   <div className="mt-4">
+//                     <button
+//                       type="button"
+//                       className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+//                       onClick={closeModal}
+//                     >
+//                       Got it, thanks!
+//                     </button>
+//                   </div>
+//                 </Dialog.Panel>
+//               </Transition.Child>
+//             </div>
+//           </div>
+//         </Dialog>
+//       </Transition>
+//     </>
+//   )
+// }
+
--- new-user-requests/src/components/NewRequestForm.jsx
+++ new-user-requests/src/components/NewRequestForm.jsx
@@ -1,28 +1,108 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid';
 
-const NewRequestForm = () => {
-  return (
-    <div className='w-full p-6 bg-white border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 text-left'>
-        <form>
-            <div className="mb-6">
-                <label for="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                <input type="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
-            </div>
-            <div className="mb-6">
-                <label for="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-                <input type="password" id="password" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-            </div>
-            <div className="flex items-start mb-6">
-                <div className="flex items-center h-5">
-                <input id="remember" type="checkbox" value="" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
+const NewRequestForm = ({ closeModal, requests, setRequests }) => {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLasttName] = useState('')
+    const [lineManager, setLineManager] = useState('')
+    const [startDate, setStartDate] = useState('')
+    const [businessArea, setBusinessArea] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let newRequest = {
+            id: uuidv4(),
+            firstName,
+            lastName,
+            lineManager,
+            startDate,
+            businessArea,
+            completed: false
+        }
+        setRequests([...requests, newRequest])
+        closeModal()
+    }
+
+    return (
+        <div className='w-full p-4 bg-white border-gray-200 rounded-lg shadow-sm dark:bg-gray-800 dark:border-gray-700 text-left'>
+            <form>
+                <div className='flex justify-start items-center gap-4'>
+                    <div className="mb-6 w-full">
+                        <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">First Name</label>
+                        <input 
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                            type="text" 
+                            id="firstName" 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            placeholder="John" 
+                            required />
+                    </div>
+                    <div className="mb-6 w-full">
+                        <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Last Name</label>
+                        <input 
+                            value={lastName}
+                            onChange={(e) => setLasttName(e.target.value)}
+                            type="text" 
+                            id="lastName" 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            placeholder="Doe" 
+                            required />
+                    </div>
                 </div>
-                <label for="remember" className="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-            </div>
-            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-        </form>
+                <div className='flex justify-start items-center gap-4'>
+                    <div className="mb-6 w-full">
+                        <label htmlFor="lineManager" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Line Manager</label>
+                        <input 
+                            value={lineManager}
+                            onChange={(e) => setLineManager(e.target.value)}
+                            type="text" 
+                            id="lineManager" 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            placeholder="Jane Doe" 
+                            required />
+                    </div>
+                    <div className="mb-6 w-full">
+                        <label htmlFor="startDate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Start Date</label>
+                        <input 
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            type="date" 
+                            id="startDate" 
+                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                            required />
+                    </div>
+                </div>
+                <div className='mb-6'>
+                    <label htmlFor="gender" className="block text-sm font-medium text-gray-700">
+                        Business Area
+                    </label>
+                    <select
+                        value={businessArea}
+                        onChange={(e) => setBusinessArea(e.target.value)}
+                        id="businessArea"
+                        name="businessArea"
+                        className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                    >
+                        <option value="" disabled hidden>Select an area</option>
+                        <option value='IT' label='IT' />
+                        <option value='Finance' label='Finance' />
+                        <option value='HR' label='HR' />
+                        <option value='Housing' label='Housing' />
+                        <option value='Care' label='Care' />
+                    </select>
+                </div>
+                <button 
+                    onClick={handleSubmit}
+                    type="submit" 
+                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        Submit
+                </button>
+            </form>
 
-    </div>
-  )
+        </div>
+    )
 }
 
 export default NewRequestForm
